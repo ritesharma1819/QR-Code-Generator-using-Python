136537dabb3c169a8200d1c502b103c528bb8bdc
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-01-01 23:27:43 +0530

    add link frame and qr frame and also add buttoms

--- qr_code_generator_using_python.py
+++ qr_code_generator_using_python.py
@@ -13,6 +13,9 @@ anchor='w').place(x=0,y=0,relwidth=1)
 
 # divide the window for user to give the message or link and add message or link label
 message_frame=Frame(window,bg='white',bd=2,relief=RAISED).place(x=50,y=100,width=480,height=380)
+ 
+link_title=Label(message_frame,text="                           Link Box                          ",font=('times new roman',20),bg='#07325A',
+fg='white').place(x=50,y=100)
 
 link_label=Label(message_frame,text='Link or Message',
 font=('Arial',20,BOLD),fg='blue',bg='white').place(x=180,y=150)
@@ -26,5 +29,18 @@ font=('times new roman',20,BOLD),fg='white',bg='#0096B4').place(x=130,y=300,widt
 
 clear_button=Button(message_frame,text='claar',
 font=('times new roman',20,BOLD),fg='white',bg='#69855D').place(x=300,y=300,width=130,height=50)
+
+# write message on screen when qr code generated or not
+message_label=Label(message_frame,text='Qr generated sucessfully !!',
+font=('times new roman',25),fg='green',bg='white').place(x=115,y=380)
+
+
+# making qr code frame 
+qr_frame=Frame(window,bg='white',bd=2,relief=RAISED).place(x=550,y=100,width=310,height=380)
+ 
+qr_title=Label(qr_frame,text="             Qr Code Box          ",font=('times new roman',20),bg='#07325A',
+fg='white').place(x=550,y=100)
+qr_code=Label(qr_frame,text='No Qr\n Available',
+font=('Arial',20),fg='white',bg='#62A586').place(x=615,y=215,width=180,height=180)
 
 window.mainloop()
