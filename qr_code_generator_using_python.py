845251d8eb4610f3cb7c212c415eac76e509f8f7
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-01-07 01:52:23 +0530

    added qr_generator function

--- qr_code_generator_using_python.py
+++ qr_code_generator_using_python.py
@@ -1,6 +1,7 @@
 from tkinter import*
+from tkinter import messagebox
 from tkinter import font
-import qrcode
+import pyqrcode
 from PIL import ImageTk,Image
 from tkinter.font import BOLD
 from resizeimage import resizeimage
@@ -15,20 +16,33 @@ window.title('Qr Code Generator')
 # window.resizable(False,False)
 
 # delartion of variable
-# var_link=StringVar()
+var_link=StringVar()
 
+
+def qr_generator():
+    if len(var_link.get())!=0:
+        global qr , photo
+        qr=pyqrcode.create(var_link.get())
+        photo=BitmapImage(data=qr.xbm(scale=8))
+    else:
+        messagebox.showinfo("qr code generator" ,message="please enter the link")
+
+def clear():
+    pass
 
 
 link_title=Label(window,text="  Link  ",font=('Arial',30,BOLD))
 link_title.grid(row=0,column=0)
 
-link_entry=Entry(window,font=('times in roman',25))
+link_entry=Entry(window,font=('times in roman',25),textvariable=var_link)
 link_entry.grid(row=0,column=1)
 
-qr_button=Button(window,text='Qr Generator',font=('Times in roman',20,BOLD),fg='white',bg='#434AA8')
+qr_button=Button(window,text='Qr Generator',font=('Times in roman',20,BOLD),
+fg='white',bg='#434AA8',command=qr_generator)
 qr_button.grid(row=0,column=2)
 
-clear_button=Button(window,text='Clear',font=('Times in roman',20,BOLD),fg='white',bg='#6D6D15')
+clear_button=Button(window,text='Clear',font=('Times in roman',20,BOLD),
+fg='white',bg='#6D6D15',command=clear)
 clear_button.grid(row=1,column=2)
 
 
